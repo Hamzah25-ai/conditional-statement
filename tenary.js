3c311a069f762ed8e1bd097fac9e8e311e2389fc
Hamzah Yusuf <yusufhamzah800@gmail.com>
2023-01-15 15:28:08 +0100

    tenary

--- tenary.js
+++ tenary.js
@@ -1,17 +1,41 @@
-let myName = "Yusuf hamzah";
-myName.length > 30
-? console.log("I have a long name")
-: console.log("I have a short name");
+let userName = "jane";
+userName ? console.log("hello " + userName) : console.log("");
 
-let mySchoolName = "Command Day Secondary School";
-console.log(mySchoolName.length)
-let schoolName = mySchoolName.length;
-let variable = 2
-const result = schoolName / variable;
-console.log(result);
-mySchoolName > 10
-? console.log("my schoool has a short name")
-: console.log("my school has a long name");
+const userQuestion = "Will I become a werewolf tonight?";
+console.log(`Jane asked: ${userQuestion}`);
 
-// use modulus to divide the length and compare if greater than 
+const randomNumber = Math.random() * 8;
+let finalStudentScore = Math.floor(randomNumber);
+console.log(finalStudentScore);
 
+switch (finalStudentScore) {
+  case 0:
+    console.log("it is certain");
+    break;
+  case 1:
+    console.log("it is decidedly so");
+    break;
+  case 2:
+    console.log("Reply hazy try again");
+    break;
+  case 3:
+    console.log("cannot predict now");
+    break;
+  case 4:
+    console.log("do not count on it");
+    break;
+  case 5:
+    console.log("my sources say no");
+    break;
+  case 6:
+    console.log("outlook not so good");
+    break;
+  case 7:
+    console.log("signs point to yes");
+    break;
+
+  default:
+    // console.log(`The eight ball answered: ${randomNumber}`);
+
+    break;
+}
